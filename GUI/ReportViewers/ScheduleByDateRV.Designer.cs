9e72fd52c17211c8423598b70b11eadd269485e1
Mai Nguyen Hoang Vu <mainguyenhoangvu2212@gmail.com>
2024-11-24 11:59:37 +0700

    edit size reports

--- GUI/ReportViewers/ScheduleByDateRV.Designer.cs
+++ GUI/ReportViewers/ScheduleByDateRV.Designer.cs
@@ -43,9 +43,9 @@
             this.pnlLearners = new Guna.UI2.WinForms.Guna2Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.lblCourses = new System.Windows.Forms.Label();
-            this.dtpFromDate = new Guna.UI2.WinForms.Guna2DateTimePicker();
+            this.dtpStartDate = new Guna.UI2.WinForms.Guna2DateTimePicker();
             this.lblDate = new System.Windows.Forms.Label();
-            this.dtpToDate = new Guna.UI2.WinForms.Guna2DateTimePicker();
+            this.dtpEndDate = new Guna.UI2.WinForms.Guna2DateTimePicker();
             this.pnlSpace15 = new Guna.UI2.WinForms.Guna2Panel();
             this.pnlSpace7 = new Guna.UI2.WinForms.Guna2Panel();
             this.pnlLineTop.SuspendLayout();
@@ -179,9 +179,9 @@
             // 
             this.pnlLearners.Controls.Add(this.label1);
             this.pnlLearners.Controls.Add(this.lblCourses);
-            this.pnlLearners.Controls.Add(this.dtpFromDate);
+            this.pnlLearners.Controls.Add(this.dtpStartDate);
             this.pnlLearners.Controls.Add(this.lblDate);
-            this.pnlLearners.Controls.Add(this.dtpToDate);
+            this.pnlLearners.Controls.Add(this.dtpEndDate);
             this.pnlLearners.Controls.Add(this.pnlSpace15);
             this.pnlLearners.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlLearners.FillColor = System.Drawing.Color.White;
@@ -217,26 +217,27 @@
             this.lblCourses.Text = "    From: ";
             this.lblCourses.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // dtpFromDate
+            // dtpStartDate
             // 
-            this.dtpFromDate.BorderColor = System.Drawing.Color.White;
-            this.dtpFromDate.BorderRadius = 5;
-            this.dtpFromDate.Checked = true;
-            this.dtpFromDate.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.dtpFromDate.CustomFormat = "dd-MM-yyyy";
-            this.dtpFromDate.Dock = System.Windows.Forms.DockStyle.Right;
-            this.dtpFromDate.FillColor = System.Drawing.Color.White;
-            this.dtpFromDate.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
-            this.dtpFromDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(50)))), ((int)(((byte)(52)))));
-            this.dtpFromDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpFromDate.Location = new System.Drawing.Point(322, 0);
-            this.dtpFromDate.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
-            this.dtpFromDate.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
-            this.dtpFromDate.Name = "dtpFromDate";
-            this.dtpFromDate.Size = new System.Drawing.Size(245, 36);
-            this.dtpFromDate.TabIndex = 11;
-            this.dtpFromDate.Tag = "Date";
-            this.dtpFromDate.Value = new System.DateTime(2024, 9, 22, 18, 8, 37, 767);
+            this.dtpStartDate.BorderColor = System.Drawing.Color.White;
+            this.dtpStartDate.BorderRadius = 5;
+            this.dtpStartDate.Checked = true;
+            this.dtpStartDate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dtpStartDate.CustomFormat = "dd-MM-yyyy";
+            this.dtpStartDate.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dtpStartDate.FillColor = System.Drawing.Color.White;
+            this.dtpStartDate.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
+            this.dtpStartDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(50)))), ((int)(((byte)(52)))));
+            this.dtpStartDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpStartDate.Location = new System.Drawing.Point(322, 0);
+            this.dtpStartDate.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
+            this.dtpStartDate.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
+            this.dtpStartDate.Name = "dtpStartDate";
+            this.dtpStartDate.Size = new System.Drawing.Size(245, 36);
+            this.dtpStartDate.TabIndex = 11;
+            this.dtpStartDate.Tag = "Date";
+            this.dtpStartDate.Value = new System.DateTime(2024, 9, 22, 18, 8, 37, 767);
+            this.dtpStartDate.ValueChanged += new System.EventHandler(this.dtpFromDate_ValueChanged);
             // 
             // lblDate
             // 
@@ -251,26 +252,27 @@
             this.lblDate.Text = " To: ";
             this.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // dtpToDate
+            // dtpEndDate
             // 
-            this.dtpToDate.BorderColor = System.Drawing.Color.White;
-            this.dtpToDate.BorderRadius = 5;
-            this.dtpToDate.Checked = true;
-            this.dtpToDate.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.dtpToDate.CustomFormat = "dd-MM-yyyy";
-            this.dtpToDate.Dock = System.Windows.Forms.DockStyle.Right;
-            this.dtpToDate.FillColor = System.Drawing.Color.White;
-            this.dtpToDate.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
-            this.dtpToDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(50)))), ((int)(((byte)(52)))));
-            this.dtpToDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpToDate.Location = new System.Drawing.Point(660, 0);
-            this.dtpToDate.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
-            this.dtpToDate.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
-            this.dtpToDate.Name = "dtpToDate";
-            this.dtpToDate.Size = new System.Drawing.Size(245, 36);
-            this.dtpToDate.TabIndex = 10;
-            this.dtpToDate.Tag = "Date";
-            this.dtpToDate.Value = new System.DateTime(2024, 9, 22, 18, 8, 37, 767);
+            this.dtpEndDate.BorderColor = System.Drawing.Color.White;
+            this.dtpEndDate.BorderRadius = 5;
+            this.dtpEndDate.Checked = true;
+            this.dtpEndDate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dtpEndDate.CustomFormat = "dd-MM-yyyy";
+            this.dtpEndDate.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dtpEndDate.FillColor = System.Drawing.Color.White;
+            this.dtpEndDate.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
+            this.dtpEndDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(50)))), ((int)(((byte)(52)))));
+            this.dtpEndDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpEndDate.Location = new System.Drawing.Point(660, 0);
+            this.dtpEndDate.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
+            this.dtpEndDate.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
+            this.dtpEndDate.Name = "dtpEndDate";
+            this.dtpEndDate.Size = new System.Drawing.Size(245, 36);
+            this.dtpEndDate.TabIndex = 10;
+            this.dtpEndDate.Tag = "Date";
+            this.dtpEndDate.Value = new System.DateTime(2024, 9, 22, 18, 8, 37, 767);
+            this.dtpEndDate.ValueChanged += new System.EventHandler(this.dtpToDate_ValueChanged);
             // 
             // pnlSpace15
             // 
@@ -322,10 +324,10 @@
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
         private Guna.UI2.WinForms.Guna2Panel pnlLearners;
-        private Guna.UI2.WinForms.Guna2DateTimePicker dtpToDate;
+        private Guna.UI2.WinForms.Guna2DateTimePicker dtpEndDate;
         private System.Windows.Forms.Label lblDate;
         private System.Windows.Forms.Label lblCourses;
-        private Guna.UI2.WinForms.Guna2DateTimePicker dtpFromDate;
+        private Guna.UI2.WinForms.Guna2DateTimePicker dtpStartDate;
         private Guna.UI2.WinForms.Guna2Panel pnlSpace10;
         private Guna.UI2.WinForms.Guna2Button btnPrint;
         private Guna.UI2.WinForms.Guna2Panel pnlSpace13;
